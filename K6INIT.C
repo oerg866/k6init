201965f9500e8d1c008599889c9ced6fa09141ca
Eric Voirin <oerg866@tototek.com>
2021-08-22 05:11:34 +0200

    Add initial WIP program sources and makefile.

---
+++ K6INIT.C
@@ -0,0 +1,154 @@
+#include <string.h>
+#include <malloc.h>
+#include <dos.h>
+
+#include "k6.h"
+
+int checkAuthenticAMD(void)
+{
+    char cpuidString[12+1];  // 3 DWORDs + 1 null term byte 
+
+    cpuidString[12] = 0x00;  // null terminator
+
+    k6_getCPUIdentifier((char _far*)cpuidString);
+    printf("CPU ID String: %s\n", cpuidString);
+
+    return(memcmp(cpuidString, "AuthenticAMD", 12) == 0);
+
+}
+
+int checkSupportedCPU(void)
+{
+    cpuidProcessorType cpu;
+
+    cpu = k6_getCPUProcessorType();
+
+    printf("CPU Family %02lx model %02lx stepping %02lx\n", 
+        cpu.family, 
+        cpu.model, 
+        cpu.stepping);
+
+    // Supported CPUs
+    // Family   Model     Stepping
+    // 5        8         C         AMD K6-II(CXT)
+    // 5        9         --        AMD K6-III
+    // 5        D         --        AMD K6-II+ / K6-IIIE+
+
+    if (cpu.family != 5) {
+        printf("Unsupported CPU family!\n");
+        return 0;
+    }
+
+    if (cpu.model == 0x08 && cpu.stepping == 0x0C)    {
+        printf("AMD K6-II (CXT) detected!\n");
+    } else if (cpu.model == 0x09) {
+        printf("AMD K6-III detected!\n");
+    } else if (cpu.model == 0x0D) {
+        printf("AMD K6-II+ / K6-IIIE+ detected!\n");
+    } else {
+        printf("Unsupported CPU Model / Stepping!\n");
+        return 0;
+    }
+
+    return 1;
+}
+
+int findLFBs(void) {
+
+    vbeInfo *vbeInfoPtr;
+    vbeModeInfo *vbeModeInfoPtr;
+    unsigned short ret;
+    int vbeVersionMajor, vbeVersionMinor;
+    char oemVersionString[16+1];
+    unsigned short i;
+    unsigned short currentMode;
+
+    printf("Attempting to find Linear Frame Buffer (LFB) region(s)...\n");
+    printf("Probing VGA BIOS for VBEs...\n");
+
+    // Allocate memory for VBE (Mode) Info structures
+
+    vbeInfoPtr = malloc(sizeof(vbeInfo));
+    vbeModeInfoPtr = malloc(sizeof(vbeModeInfo));
+
+    if ((vbeInfoPtr == NULL) || (vbeModeInfoPtr == NULL)) {
+        printf("ERROR> could not allocate memory for VBE Info Structures.\n");
+        goto error;
+    }
+
+    // Init the memory
+
+    memset(vbeInfoPtr, 0, sizeof(vbeInfo));
+    memset(vbeModeInfoPtr, 0, sizeof(vbeModeInfo));
+    memset(oemVersionString, 0, sizeof(oemVersionString));
+
+    // A VBE Info Block request must happen with the target block's 
+    // Signature field set to "VBE2"
+
+    memcpy(vbeInfoPtr->vbeSignature, "VBE2", 4);
+
+    ret = k6_getVBEInfoBlock(vbeInfoPtr);
+
+    if (ret != 0x004F) {
+        printf("VESA call failed. Cannot automatically find LFBs.\n");
+        goto error;
+    }
+
+    vbeVersionMajor = vbeInfoPtr->vbeVersion >> 8;
+    vbeVersionMinor = vbeInfoPtr->vbeVersion & 0xFF;
+
+    printf("VESA BIOS found, Signature: %.4s, VESA Version %.1x.%02x\n",
+        vbeInfoPtr->vbeSignature,
+        vbeVersionMajor,
+        vbeVersionMinor);
+
+    // Copy out OEM version string (Yay far pointers)
+
+    _fstrncpy((char _far*)oemVersionString, vbeInfoPtr->oemStringPtr, 16);
+
+    printf("VESA BIOS OEM Version String: %s\n", oemVersionString);
+
+    // If we have VESA version before 2.xx, we have to stop because
+    // VESA only introduced LFBs starting at 2.xx. 
+
+    if (vbeVersionMajor < 2) {
+        printf("VBE Version before 2.00. No LFB detection possible.\n");
+        goto error;
+    }
+
+    // Query all video modes to find those with LFBs. 
+
+    printf("Querying VESA modes to find LFB address...\n"); 
+
+    // TODO: This is broken, the end of list marker 0xffff 
+    // is not found on my GeForce 2 GTS?
+
+//    for (i = 0; vbeInfoPtr->videoModeListPtr[i] != 0xFFFF; i++) {
+
+    for (i = 0; i < 16; i++) {
+        currentMode = vbeInfoPtr->videoModeListPtr[i]; 
+
+        printf("Found Mode %03hx, ", currentMode);
+        ret = k6_getVBEModeInfo(currentMode, 
+            (vbeModeInfo _far*) vbeModeInfoPtr);
+
+        printf("ret: %04hx - Resolution: %ux%u - BPP: %u\n",
+            ret,
+            vbeModeInfoPtr->width,
+            vbeModeInfoPtr->height,
+            vbeModeInfoPtr->bpp);
+
+        // ret = return code from query call 
+    } 
+
+    free(vbeModeInfoPtr); 
+    free(vbeInfoPtr); 
+    return 1;
+
+// TODO: Make this more elegant :/
+error:
+    free(vbeModeInfoPtr); 
+    free(vbeInfoPtr); 
+    return -1;
+}
+
---
+++ MAIN.C
@@ -0,0 +1,82 @@
+
+/*  ************************************************************************  *
+ *                                   K6INIT                                   *
+ *     AMD K6-II(CXT) / K6-II+ / K6-III+ initialization driver for MS-DOS     *
+ *                            (C) 2021 Eric Voirin                            *
+ *  ************************************************************************  */
+
+#include <dos.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <keepc.h>
+#include <standard.h>
+
+#include "k6.h"
+
+/*  Hard error trap... I have no idea why this is here, but the
+    driver hybrid ASM+C example from DOS internals had this in it,
+    so I am keeping it :P */
+
+static int _far HardErrorTrap (void)
+{
+    return (_HARDERR_FAIL);
+}
+
+static char k_program_name [] = "K6INIT";
+static int k_version_major = 0;
+static int k_version_minor = 1;
+static char k_copyright_text [] = "(C) 2021 Eric \"oerg866\" Voirin";
+static char k_contact_info [] = "Contact me on Discord: EricV#9999";
+
+/*  Message output  */
+
+static void PutError (char *str, ...);
+
+/*  ========================================================================  */
+
+int main (int argc, char **argv)
+{
+    printf("%s Version %d.%02d\n", k_program_name, k_version_major, k_version_minor);
+    printf("%s\n", k_copyright_text);
+    printf("%s\n", k_contact_info); 
+    printf("===============================================================================\n");
+
+    if (!checkAuthenticAMD()) {
+        printf("You don't have an AMD CPU. Aborting.\n"); 
+        goto cleanup;
+    }
+
+    if (!checkSupportedCPU()) {
+        printf("Aborting\n");
+        goto cleanup; 
+    }
+
+    findLFBs();
+
+    /*  Parse the command line.  */
+
+    signal(SIGINT, SIG_IGN);
+    _harderr(HardErrorTrap);
+
+    /*  Tidy up.  */
+
+cleanup:
+    CloseAllFiles ();
+    FreeEnvironment ();
+    ExitKeepC (0, 0);
+}
+
+static void PutError (char *str, ...)
+{
+    va_list va_start (argptr, str);
+    printf "\n%s error:  ", k_program_name);
+    vprintf(str, argptr);
+    printf("\n");
+    va_end(argptr);
+}
+
+/*  ************************************************************************  */
+
